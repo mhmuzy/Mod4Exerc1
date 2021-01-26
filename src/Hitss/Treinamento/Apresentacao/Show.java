9f7a76b70887690294230128070c19bf8999a49c
marcio henrique <mhmuzy@embratel.com.br>
2021-01-26 15:00:24 -0300

    Finalização da Criação do Programa [Treinamento]

--- src/Hitss/Treinamento/Apresentacao/Show.java
+++ src/Hitss/Treinamento/Apresentacao/Show.java
@@ -5,6 +5,17 @@ public class Show {
 		System.out.println("");
 		System.out.println("     Hitss Treinamento");
 		System.out.println("");
-		System.out.println("Prezado(a) Usuário(a), estamos em desenvolvimento no momento.");
+		System.out.println("Segue a seqüência.");
+		System.out.println("");
+		for (int i = 1; i <= 100; i++) {
+				System.out.println("Número: " + i + " Raiz Quadrada: " + Math.sqrt(i));
+			
+
+			
+			if (i == 100) {
+				System.out.println("");
+				System.out.println("Fim do programa.");
+			}
+		}
 	}
 }
